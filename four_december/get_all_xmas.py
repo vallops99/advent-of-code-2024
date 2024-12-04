aaeb30f9642a86aa77b4cb7fe4c9c3967547d1ba
Valerio Farrotti <valerio.farrotti@conio.com>
2024-12-04 11:13:01 +0100

    Submitted fourth quiz (part 1)

---
+++ four_december/get_all_xmas.py
@@ -0,0 +1,45 @@
+import re
+from four_december.utils.utils import get_data
+
+
+def get_all_xmas(rows: list[str]) -> int:
+    xmas_counter = 0
+    for idx, row in enumerate(rows):
+        # Horizontal
+        xmas_counter += len(re.findall(r"XMAS", row))
+        xmas_counter += len(re.findall(r"SAMX", row))
+        for subidx, char in enumerate(row):
+            # Vertical
+            if idx + 3 < len(rows):
+                vertical = (
+                    char
+                    + rows[idx + 1][subidx]
+                    + rows[idx + 2][subidx]
+                    + rows[idx + 3][subidx]
+                )
+                xmas_counter += "XMAS" == vertical or "SAMX" == vertical
+                # Diagonal right
+                if subidx + 3 < len(row):
+                    diagonal = (
+                        char
+                        + rows[idx + 1][subidx + 1]
+                        + rows[idx + 2][subidx + 2]
+                        + rows[idx + 3][subidx + 3]
+                    )
+                    xmas_counter += "XMAS" == diagonal or "SAMX" == diagonal
+                # Diagonal left
+                if subidx - 3 >= 0:
+                    diagonal = (
+                        char
+                        + rows[idx + 1][subidx - 1]
+                        + rows[idx + 2][subidx - 2]
+                        + rows[idx + 3][subidx - 3]
+                    )
+                    xmas_counter += "XMAS" == diagonal or "SAMX" == diagonal
+
+    return xmas_counter
+
+
+if __name__ == "__main__":
+    # print(get_data())
+    print("All XMAS are: ", get_all_xmas(get_data()))
---
+++ four_december/test/test_get_all_xmas.py
@@ -0,0 +1,19 @@
+import unittest
+from four_december.get_all_xmas import get_all_xmas
+
+
+class TestGetAllXmas(unittest.TestCase):
+    def test_get_all_xmas_example(self):
+        vector = [
+            "....XXMAS.",
+            ".SAMXMS...",
+            "...S..A...",
+            "MSA.A.MS.X",
+            "XMASAMXAMM",
+            "XXAMMXXAMA",
+            "SMSMSASXSS",
+            "SAXAMASAAA",
+            "MAMMMXMMMM",
+            "MXMXAXMASX",
+        ]
+        self.assertEqual(get_all_xmas(vector), 18)
